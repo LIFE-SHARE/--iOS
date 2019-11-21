849b2f72f69ac7ce0bd542138e0371109d6e4a33
DAEUN28 <acone1128@gmail.com>
2019-11-21 14:10:58 +0900

    initial commit

---
+++ LIFE-SHARE/LIFE-SHARE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LIFE-SHARE
+//
+//  Created by baby1234 on 2019/11/21.
+//  Copyright © 2019 baby1234. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
